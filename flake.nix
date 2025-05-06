cdd5bcdb8278ef06c9eb55ab7faa0f62717be188
Valentin Leon Stöcker <valentin@stoeckermail.de>
2025-05-07 01:01:02 +0200

    add readline

--- flake.nix
+++ flake.nix
@@ -5,20 +5,27 @@
   outputs =
     { self, nixpkgs, ... }:
     let
-      overlay = (prev: final: {
-        libconfig = final.libconfig.overrideAttrs (old: {
-          version = "1.8";
-          src = final.fetchurl {
-            url = "https://hyperrealm.github.io/libconfig/dist/libconfig-1.8.tar.gz";
-            hash = "sha256-BR4V3Q6QfESQXzF5M/VIcxTypW6MZybIMEzpkIhIUKo=";
-          };
-        });
-      });
+      overlay = (
+        prev: final: {
+          libconfig = final.libconfig.overrideAttrs (old: {
+            version = "1.8";
+            src = final.fetchurl {
+              url = "https://hyperrealm.github.io/libconfig/dist/libconfig-1.8.tar.gz";
+              hash = "sha256-BR4V3Q6QfESQXzF5M/VIcxTypW6MZybIMEzpkIhIUKo=";
+            };
+          });
+        }
+      );
       pkgs = nixpkgs.legacyPackages.x86_64-linux.extend overlay;
     in
     {
       packages.x86_64-linux = {
-        inherit (pkgs) hello cowsay libconfig;
+        inherit (pkgs)
+          hello
+          cowsay
+          libconfig
+          readline
+          ;
       };
 
       hydraJobs = {
@@ -28,4 +35,3 @@
       };
     };
 }
-
