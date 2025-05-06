8763a5611d98322450e21fdd9e22e9deab0359b2
Valentin Leon Stöcker <valentin@stoeckermail.de>
2025-05-06 04:22:30 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      packages.x86_64-linux = {
+        inherit (pkgs) hello;
+      };
+
+      hydraJobs = {
+        inherit (self)
+          packages
+          ;
+      };
+    };
+}
