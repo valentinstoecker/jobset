f989d909c8e190b0d92a13789566084e6936c4ea
valentinstoecker <valentin@stoeckermail.de>
2025-05-06 20:34:13 +0200

    fake hash

--- flake.nix
+++ flake.nix
@@ -5,19 +5,19 @@
   outputs =
     { self, nixpkgs, ... }:
     let
-      overlay = prev: final: {
+      overlay = (prev: final: {
         libconfig = prev.libconfig.overrideAttrs (old: {
           src = prev.fetchurl {
-            url = "http://hyperrealm.github.io/libconfig/dist/libconfig-1.8.tar.gz";
-            hash = "";
+            url = "https://hyperrealm.github.io/libconfig/dist/libconfig-1.8.tar.gz";
+            hash = "00000000000000000000000000000000000000000000000000000000";
           };
         });
-      };
+      });
       pkgs = nixpkgs.legacyPackages.x86_64-linux.extend overlay;
     in
     {
       packages.x86_64-linux = {
-        inherit (pkgs) hello cowsay ffmpeg;
+        inherit (pkgs) hello cowsay ffmpeg libconfig;
       };
 
       hydraJobs = {
@@ -27,3 +27,4 @@
       };
     };
 }
+
