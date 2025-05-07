0cb14bcd071cbcdfb0495ae26785db6f916bed79
Valentin Leon Stöcker <valentin@stoeckermail.de>
2025-05-07 09:12:35 +0200

    shorten

--- flake.nix
+++ flake.nix
@@ -5,17 +5,15 @@
   outputs =
     { self, nixpkgs, ... }:
     let
-      overlay = (
-        prev: final: {
-          libconfig = final.libconfig.overrideAttrs (old: {
-            version = "1.8";
-            src = final.fetchurl {
-              url = "https://hyperrealm.github.io/libconfig/dist/libconfig-1.8.tar.gz";
-              hash = "sha256-BR4V3Q6QfESQXzF5M/VIcxTypW6MZybIMEzpkIhIUKo=";
-            };
-          });
-        }
-      );
+      overlay = prev: final: {
+        libconfig = final.libconfig.overrideAttrs (old: {
+          version = "1.8";
+          src = final.fetchurl {
+            url = "https://hyperrealm.github.io/libconfig/dist/libconfig-1.8.tar.gz";
+            hash = "sha256-BR4V3Q6QfESQXzF5M/VIcxTypW6MZybIMEzpkIhIUKo=";
+          };
+        });
+      };
       pkgs = nixpkgs.legacyPackages.x86_64-linux.extend overlay;
     in
     {
