03f9ea482fc4fd9afaf109e1829073afcbe1f35b
Valentin Leon Stöcker <valentin@stoeckermail.de>
2025-07-24 03:13:33 +0200

    rem libconf

--- flake.nix
+++ flake.nix
@@ -5,16 +5,19 @@
   outputs =
     { self, nixpkgs, ... }:
     let
-      overlay = prev: final: {
-        libconfig = final.libconfig.overrideAttrs (old: {
-          version = "1.8";
-          src = final.fetchurl {
-            url = "https://hyperrealm.github.io/libconfig/dist/libconfig-1.8.tar.gz";
-            hash = "sha256-BR4V3Q6QfESQXzF5M/VIcxTypW6MZybIMEzpkIhIUKo=";
-          };
-        });
-      };
-      pkgs = nixpkgs.legacyPackages.x86_64-linux.extend overlay;
+      /*
+        overlay = prev: final: {
+          libconfig = final.libconfig.overrideAttrs (old: {
+            version = "1.8";
+            src = final.fetchurl {
+              url = "https://hyperrealm.github.io/libconfig/dist/libconfig-1.8.tar.gz";
+              hash = "sha256-BR4V3Q6QfESQXzF5M/VIcxTypW6MZybIMEzpkIhIUKo=";
+            };
+          });
+        };
+      */
+      pkgs = nixpkgs.legacyPackages.x86_64-linux # .extend overlay
+      ;
     in
     {
       packages.x86_64-linux = {
